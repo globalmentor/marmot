47ce5c78373467e0de0daf6e49b749056258c0f4
Garret Wilson <garret@globalmentor.com>
2003-03-06 19:28:06 +0000

    designed RDF-based configuration file framework

---
+++ src/com/globalmentor/marmot/MarmotConstants.java
@@ -0,0 +1,35 @@
+package com.globalmentor.marmot;
+
+import java.io.*;
+import java.net.*;
+import com.garretwilson.lang.*;
+
+/**Constant values used by Marmot.
+@author Garret Wilson
+*/
+public interface MarmotConstants
+{
+
+		//XML/RDF constants
+
+	/**The recommended prefix to the Marmot vocabulary namespace.*/
+	public final static String MARMOT_NAMESPACE_PREFIX="marmot";
+
+	/**The URI to the Marmot namespace.*/
+	public final static URI MARMOT_NAMESPACE_URI=URI.create("http://globalmentor.org/namespaces/2003/marmot#");
+
+		//configuration file constants
+
+	/**The name of the configuration directory.*/
+	public final static String CONFIGURATION_DIRECTORY_NAME=".marmot";
+	
+	/**The configuration directory.*/
+	public final static File CONFIGURATION_DIRECTORY=new File(System.getProperty(SystemConstants.USER_HOME), CONFIGURATION_DIRECTORY_NAME);
+
+	/**The filename of the configuration file.*/
+	public final static String CONFIGURATION_FILENAME="configuration.rdf";
+
+	/**The file in which the the configuration information are stored.*/
+	public final static File CONFIGURATION_FILE=new File(CONFIGURATION_DIRECTORY, CONFIGURATION_FILENAME);
+	
+}
